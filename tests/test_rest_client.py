dce011e1cac94748a8cace4894f4120f63d1623d
calvinchan <calvinchancan@gmail.com>
2023-01-11 23:03:44 -0500

    Add tests for RestAuthTests

--- tests/test_rest_client.py
+++ tests/test_rest_client.py
@@ -11,10 +11,62 @@ from mintapi.rest import RESTClient
 
 class RestAuthTests(unittest.TestCase):
     def test_header_updates(self):
-        pass
+        rest_client = RESTClient()
+        self.assertIsNone(rest_client.session.headers.get("authorization"))
+        self.assertIsNone(rest_client.session.headers.get("cookie"))
+        api_key = "API KEY"
+        cookie = "mint.authid=1234567890"
+
+        rest_client.authorize(api_key=api_key, cookies=cookie)
+        self.assertEqual(api_key, rest_client.session.headers.get(("authorization")))
+        self.assertEqual(cookie, rest_client.session.headers.get(("cookie")))
+
+    def test_header_updates_with_cookies_list(self):
+        rest_client = RESTClient()
+        self.assertIsNone(rest_client.session.headers.get("authorization"))
+        self.assertIsNone(rest_client.session.headers.get("cookie"))
+        api_key = "API KEY"
+        cookies = [
+            {
+                "domain": ".intuit.com",
+                "httpOnly": True,
+                "name": "mint.authid",
+                "path": "/",
+                "sameSite": "None",
+                "secure": True,
+                "value": "1234567890",
+            }
+        ]
+        rest_client.authorize(api_key=api_key, cookies=cookies)
+        self.assertEqual(api_key, rest_client.session.headers.get(("authorization")))
+        self.assertEqual(None, rest_client.session.headers.get(("cookie")))
 
     def test_cookie_updates(self):
-        pass
+        original_cookie = {
+            "domain": ".intuit.com",
+            "httpOnly": True,
+            "name": "mint.authid",
+            "path": "/",
+            "sameSite": "None",
+            "secure": True,
+            "value": "1234567890",
+        }
+        rest_client = RESTClient(api_key="API Key", cookies=[original_cookie])
+
+        updated_cookie = {
+            "domain": ".intuit.com",
+            "httpOnly": True,
+            "name": "mint.authid",
+            "path": "/",
+            "sameSite": "None",
+            "secure": True,
+            "value": "0987654321",
+        }
+
+        rest_client.update_cookies(cookies=[updated_cookie])
+        self.assertEqual(
+            updated_cookie["value"], rest_client.session.cookies.get("mint.authid")
+        )
 
 
 class RestRequestHandlingTests(unittest.TestCase):
